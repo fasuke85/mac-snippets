3f303571bc8b19a9c96921c098c2b31d23ecef75
Johannes Fauske <johannfa@gmail.com>
2020-02-19 15:19:39 +0100

    install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,18 @@
-brew install nano
-brew install wget
+if ! [ "$(command -v brew)" ]; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
+#-------------------------
+#         Nano
+#-------------------------
+
+brew install nano
+
+# syntax highlighting
+brew install wget
 curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
 
+# set nano as default editor
 if grep -q "EDITOR" ~/.zshrc; 
   then
     echo "zshrc already updated"
--- install/homebrew.sh
+++
@@ -1,3 +0,0 @@
-if ! [ "$(command -v brew)" ]; then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
